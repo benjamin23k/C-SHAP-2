f13f08d5b3f68ebbd5433f31ff080b9b567c3c12
Benjamin23k <wilsonbenjaminrosa@gmail.com>
2026-07-18 16:57:19 -0400

    Update ApartmentRepository.cs

--- Dues.Infrastructure/Repositories/ApartmentRepository.cs
+++ Dues.Infrastructure/Repositories/ApartmentRepository.cs
@@ -5,7 +5,7 @@ using Dues.Infrastructure.Interfaces;
 
 namespace Dues.Infrastructure.Repositories
 {
-    public class ApartmentRepository : BaseRepository<Apartament>, IApartmentRepository
+    public class ApartmentRepository : BaseRepository<Apartment>, IApartmentRepository
     {
         public ApartmentRepository(DuesContext db) : base(db)
         {
