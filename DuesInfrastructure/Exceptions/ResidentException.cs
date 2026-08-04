40fed5e60d9dd6cb9c605ae402c519aec9758828
Benjamin23k <wilsonbenjaminrosa@gmail.com>
2026-08-04 19:18:51 -0400

    Delete DuesInfrastructure directory

--- DuesInfrastructure/Exceptions/ResidentException.cs
+++
@@ -1,9 +0,0 @@
-namespace Dues.Infrastructure.Exceptions
-{
-    public class ResidentException : Exception
-    {
-        public ResidentException() { }
-        public ResidentException(string message) : base(message) { }
-        public ResidentException(string message, Exception innerException) : base(message, innerException) { }
-    }
-}
